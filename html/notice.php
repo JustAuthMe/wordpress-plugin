79022ee236eaca7e3db8f57b33633f04c1672356
PHPeter <phpeter@phpeter.fr>
2020-11-23 21:29:58 +0100

    Replace cross by times

--- html/notice.php
+++ html/notice.php
@@ -2,7 +2,7 @@
 if (isset($notice_type)): ?>
 <div id="jam-notice" class="jam-notice jam-notice-<?php echo $notice_type ?>">
     <?php echo $notice_message ?>
-    <div class="jam-notice-dismiss" onclick="closeJamNotice()" title="<?php _e('Close') ?>">&cross;</div>
+    <div class="jam-notice-dismiss" onclick="closeJamNotice()" title="<?php _e('Close') ?>">&times;</div>
 </div>
 <?php endif ?>
 <script type="text/javascript">
